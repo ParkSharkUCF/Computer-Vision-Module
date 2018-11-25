3e45bf9824357d206a82cde023b09d9e7f05d5cc
Keegan Conway <conwaykc@knights.ucf.edu>
2018-11-25 13:27:22 -0500

    added new scripts to sensor start script

--- bluetooth/main.py
+++ bluetooth/main.py
@@ -3,5 +3,8 @@ from time import sleep
 
 while(1):
     #os.system("python3 sensor1.py & python3 sensor2.py & python3 sensor3.py")
+    #os.system("python3 thresh1.py & python3 thresh2.py & python3 thresh3.py")
+    #os.system("python3 bat1.py & python3 bat2.py & python3 bat3.py")
+    #os.system("python3 id1.py & python3 id2.py & python3 id3.py")
     os.system("python ~/Desktop/detection/yolo_take2/yolo.py -c ~/Desktop/detection/yolo_take2/yolov3.cfg -w ~/Desktop/detection/yolo_take2/yolov3.weights -cl ~/Desktop/detection/yolo_take2/yolov3.txt")
     sleep(10)
